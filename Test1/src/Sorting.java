54cbea63fcf9a8f9b0ce1edb982fd9b6d4479a13
nishamoni <monsha323@gmail.com>
2018-04-07 13:59:30 +0530

    for sorting names by order

--- Test1/src/Sorting.java
+++ Test1/src/Sorting.java
@@ -1,8 +1,34 @@
+import java.util.Scanner;
 
 public class Sorting {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int x;
+		String temp;
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter nunber of names:");
+		x = s.nextInt();
+		String names[] = new String[x];
+		Scanner s1 = new Scanner(System.in);
+		System.out.println("Enter all the names:");
+		for (int i = 0; i < x; i++) {
+			names[i] = s1.nextLine();
+		}
+		for (int i = 0; i < x; i++) {
+			for (int j = i + 1; j < x; j++) {
+				if (names[i].compareTo(names[j]) > 0) {
+					temp = names[i];
+					names[i] = names[j];
+					names[j] = temp;
+				}
+				
+			}
+
+		}
+		for (int i = 0; i < x; i++) {
+			System.out.println(names[i]);
+
+		}
 
 	}
 
